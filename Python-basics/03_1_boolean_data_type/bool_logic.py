38c63a997026435fbe39bcee7d51a265cf59b693
txgux <anshy0824@gmail.com>
2026-02-12 00:39:51 +0900

    조건문 불 자료형 예제 업로드

---
+++ Python-basics/03_1_boolean_data_type/bool_logic.py
@@ -0,0 +1,15 @@
+# 논리 연산자; not
+print(not True)
+print(not False)
+
+x = 10
+under_20 = x < 20  # under_20 = (< 20)
+print("under_20:", under_20)
+print("not under_20:", not under_20)
+
+# 논리 연산자; and, or
+print(True and True)
+
+print(True or True)
+print(True or False)
+print(False or True)
